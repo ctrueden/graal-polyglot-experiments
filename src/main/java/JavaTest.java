a39838af8da0e78ba49000cc0fe5289a74bce165
Curtis Rueden <ctrueden@wisc.edu>
2018-05-15 16:20:33 -0500

    Format leading whitespace

--- src/main/java/JavaTest.java
+++ src/main/java/JavaTest.java
@@ -1,21 +1,21 @@
-import org.graalvm.polyglot.*;
 import java.io.PrintStream;
 import java.util.Set;
 
+import org.graalvm.polyglot.Context;
+
 public class JavaTest {
 
-public static void main(String[] args) {
+	public static void main(final String[] args) {
 
-    PrintStream output = System.out;
-    Context context = Context.newBuilder().allowAllAccess(true).build();
-    Set<String> languages = context.getEngine().getLanguages().keySet();
-    output.println("Current Languages available in GraalVM: " + languages);
+		final PrintStream output = System.out;
+		final Context context = Context.newBuilder().allowAllAccess(true).build();
+		final Set<String> languages = context.getEngine().getLanguages().keySet();
+		output.println("Current Languages available in GraalVM: " + languages);
 
-    System.out.println("Java: Hello World");
+		System.out.println("Java: Hello World");
 
-    context.eval("js","print('JavaScript: Hello World')");
-    context.eval("R", "print('R: Hello World');");
-    context.eval("python", "print('Python: Hello World');");
-  }
+		context.eval("js", "print('JavaScript: Hello World')");
+		context.eval("R", "print('R: Hello World');");
+		context.eval("python", "print('Python: Hello World');");
+	}
 }
-
