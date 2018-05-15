23d72931dcb3bf56914a3192bf6be3aca5e605c6
Curtis Rueden <ctrueden@wisc.edu>
2018-05-15 16:33:55 -0500

    JavaTest: show off more features
    * Ruby language
    * Passing large data reference across languages
    * Start at using JS SharedArrayBuffer

--- src/main/java/JavaTest.java
+++ src/main/java/JavaTest.java
@@ -1,21 +1,46 @@
-import java.io.PrintStream;
 import java.util.Set;
 
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
 
 public class JavaTest {
 
 	public static void main(final String[] args) {
 
-		final PrintStream output = System.out;
-		final Context context = Context.newBuilder().allowAllAccess(true).build();
+		final Context context = Context.newBuilder() //
+			.option("js.shared-array-buffer", "true") //
+			.allowAllAccess(true) //
+			.build();
 		final Set<String> languages = context.getEngine().getLanguages().keySet();
-		output.println("Current Languages available in GraalVM: " + languages);
+		System.out.println("Current Languages available in GraalVM: " + languages);
 
-		System.out.println("Java: Hello World");
+		final double[] bigArray = new double[300 * 1000 * 1000];
 
-		context.eval("js", "print('JavaScript: Hello World')");
+		// Java
+		System.out.println("Java: Hello World: " + bigArray.length);
+
+		// JavaScript
+		context.getBindings("js").putMember("bigArray", bigArray);
+		context.eval("js", "print('JavaScript: Hello World: ' + bigArray.length)");
+
+		final Value buf = context.eval("js",
+			"buf = new SharedArrayBuffer(1000 * 1000 * 1000)");
+		System.out.println("SharedArrayBuffer object = " + buf.getClass());
+		// TODO: How to use this from Java?
+
+		// NB: For the remaining languages, we use the polyglot bindings.
+		context.getPolyglotBindings().putMember("bigArray", bigArray);
+
+		// Python
+		context.eval("python", //
+			"import polyglot\n" + //
+			"big_array = polyglot.import_value('bigArray')\n" + //
+			"print('Python: Hello World: ' + str(len(big_array)))");
+
+		// Ruby
+		context.eval("ruby", "puts('Ruby: Hello World')");
+
+		// R
 		context.eval("R", "print('R: Hello World');");
-		context.eval("python", "print('Python: Hello World');");
 	}
 }
